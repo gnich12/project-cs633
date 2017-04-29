cbe8cb72eb4147fe735397830c06bd4f8c4f3113
gnich12 <jinto12@gmail.com>
2017-04-28 18:16:32 -0700

    fixed logic to retrieve reviews on business profile. and configuration files to connect to clear db

--- DAO/db_config.js
+++ DAO/db_config.js
@@ -3,10 +3,10 @@
  */
 var db_config = {
     config:{
-        host:'localhost',
-        user:'root',
-        password:'123456',
-        database:'production' //'termproj_dev'
+        host:'us-cdbr-iron-east-04.cleardb.net',
+        user:'be3ec741b80552',
+        password:'ee722cd2',
+        database:'heroku_95b803e0fd13b34'  //'termproj_dev'
     }
 };
 
